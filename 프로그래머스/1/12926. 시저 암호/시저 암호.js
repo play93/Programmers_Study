e03a7fe00b2cadc7b053b031ed764afbed726b1e
홍연주 <95739833+play93@users.noreply.github.com>
2024-09-25 09:56:00 +0900

    [level 1] Title: 시저 암호, Time: 5.16 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,22 +1,4 @@
 function solution(s, n) {
-    let answer = '';
-    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // 대문자 배열
-    const lower = 'abcdefghijklmnopqrstuvwxyz'; // 소문자 배열
-    
-    for(let i = 0; i <s.length; i++){
-        let currentChar = s[i]
-        if (upper.includes(currentChar)){
-            let index = upper.indexOf(currentChar)
-            let newIndex = (index + n) % 26
-            answer += upper[newIndex]
-        }else if(lower.includes(currentChar)){
-            let index = lower.indexOf(currentChar)
-            let newIndex = (index + n) % 26
-            answer += lower[newIndex]
-        }else if(currentChar === " "){
-            answer += " ";
-        }
-    }
-    
+    var answer = '';
     return answer;
 }
