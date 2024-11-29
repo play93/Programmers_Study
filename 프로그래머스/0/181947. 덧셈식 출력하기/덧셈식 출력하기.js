ccccbd849dbc088b3789b440132bfdb5dda440e3
홍연주 <95739833+play93@users.noreply.github.com>
2024-11-29 11:18:47 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 66.90 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    const firstNum = Number(input[0])
+    const secondNum = Number(input[1])
+    const sumNum = firstNum+secondNum
+    console.log (firstNum + " + " + secondNum + " = " + sumNum)
+});
