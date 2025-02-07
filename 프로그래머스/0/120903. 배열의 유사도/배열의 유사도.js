ba5e275f83f575a4824da8a722348dc2c3d35113
홍연주 <95739833+play93@users.noreply.github.com>
2025-02-07 17:11:51 +0900

    [level 0] Title: 배열의 유사도, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,9 @@
+function solution(s1, s2) {
+    var answer = 0;
+    for(i=0;i<s1.length;i++){
+        for(j=0;j<s2.length;j++){
+            if(s1[i] === s2[j]) {answer++;}
+        }
+    }
+    return answer;
+}
